207d13a3e01c871497cb818ae43200ca14ddb750
ysncyhn <test@test.com>
2025-04-09 19:51:16 +0300

    third commit

--- struct.c
+++ struct.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#include<locale.h>
 
 struct Ogrenci {
    char ad[50];
    char soyad[50];
    int yas;
    float not_ort;
-}ogrn;
-void main(){
-	struct Ogrenci ogrenci1;
-	ogrn ogrenci2 = {"Ahmet", "Yýlmaz", 22, 75.5};
-	ogrenci1.ad = "Yasin";
-	ogrenci1.soyad = "Ceyhan";
-	ogrenci1.yas = 23;
-	ogrenci1.not_ort = 75;
-	
-	
+};
+
+int main() {
+	setlocale(LC_ALL, "Turkish"); 
+    struct Ogrenci ogrenci1;
+    struct Ogrenci ogrenci2 = {"Ahmet", "Yýlmaz", 22, 75.5};
+
+    // String kopyalama
+    strcpy(ogrenci1.ad, "Yasin");
+    strcpy(ogrenci1.soyad, "Ceyhan");
+    ogrenci1.yas = 23;
+    ogrenci1.not_ort = 75;
+
+    // Yazdýrma
+    printf("Ogrenci1 -> %s %s | Yas: %d | Not Ort: %.2f\n", 
+            ogrenci1.ad, ogrenci1.soyad, ogrenci1.yas, ogrenci1.not_ort);
+
+    printf("Ogrenci2 -> %s %s | Yas: %d | Not Ort: %.2f\n", 
+            ogrenci2.ad, ogrenci2.soyad, ogrenci2.yas, ogrenci2.not_ort);
+
+    return 0;
 }
+
